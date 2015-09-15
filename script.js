ea7b28adb736fdc19e59b073ff4797930273b266
Rick Wagner <rickw@fireoverheaven.com>
2015-09-15 14:17:22 -0700

    Added HTML doc and external JavaScript.

---
+++ script.js
@@ -0,0 +1,139 @@
+// STEP 1
+// var someMonth;
+// function currentMonth        // function to return current month
+// var CURRENT_MONTH 	        // a constant
+// var summerMonths; 		    // an array of summer months
+// function myLibrary 	        // a function  
+
+// STEP 2
+// 4.125                   // numeric literal expression
+// "Welcome to FEWD 2"     // string literal expression
+// true                    // boolean literal expression
+// null                    // null literal expression
+
+// STEP 3
+// var aComplexExpression  = 7 + (3 * 3) - 6;
+// var anotherComplexExpression  = aComplexExpression + 5;
+
+// STEP 4
+// var strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, bolMayWeContactYou
+
+// STEP 5
+// var strFirstName;
+// var strLastName;
+// var strAddress;
+// strFirstName = "Rick";
+// strLastName = "Wagner";
+// strAddress = "4422 McClintock Street";
+
+// var strCity = "San Diego";
+// var strState = "CA";
+// var intZipCode = 92116;
+
+// var intYourAge = 56, strReferralSource = "Course Catalog", bolMayWeContactYou = true;
+
+// STEP 6
+// var life = 42 + " is the answer.";
+// console.log(life);
+
+// var aThing = true + " dat";
+// console.log(aThing);
+
+// var whatever = 13 + false;
+// console.log(whatever);
+
+// STEP 7
+// console.log(hoistTest);
+// var hoistTest = "hoisted";
+
+// STEP 8
+// Not valid. Fixed by escaping the single quote in I'm.
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString); 
+
+// STEP 9
+// var test = null;
+// console.log(test);
+
+// var test2;
+// console.log(test2);
+
+// STEP 10
+// console.log(typeof "Rick Wagner");
+
+// console.log(typeof 875.25);
+
+// console.log(typeof false);
+
+// console.log(typeof [5,6,7,8]);
+
+// console.log(typeof test3);
+
+// STEP 11
+// console.log("Hello" + " Rick Wagner" + ", welcome to the JavaScript class!");
+ 
+// STEP 12
+// var name = "Rick Wagner";
+// console.log("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 13
+// var name = "Rick Wagner";
+// var course = "JavaScript";
+// console.log("Hello " + name + ", welcome to the " + course + " class!");
+
+// STEP 14
+// var name = "Rick Wagner";
+// var course = "JavaScript";
+// console.log("Hello " + name + "." + "\nWelcome to the " + course + " class!"); 
+
+// STEP 15
+// var name = prompt('Please enter your name.');
+// var course = "JavaScript";
+// console.log("Hello " + name + "." + "\nWelcome to the " + course + " class!");
+
+// STEP 16
+// var name = prompt('Please enter your name.');
+// var course = prompt('Please enter the course name.');
+// console.log("Hello " + name + "." + "\nWelcome to the " + course + " class!");
+
+// STEP 17
+// var x = 10;
+// var y = 20;
+// console.log(x + y);
+
+// STEP 18
+// var x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 19
+// var x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+// var x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+// STEP 21
+// var x = 5      
+// var y = 2
+// console.log(x==1 || y==2);
+
+// STEP 22
+// var a = 7      
+// var b = 5      
+// console.log(a==7 && b==4);
+
+// STEP 23
+// var widget = new Object();
+// console.log(typeof widget);
+
+// STEP 24
+// var widget = new Object();
+// console.log(widget instanceof Object);
+
+// STEP 25
+// var widget = new Object();
+// console.log(widget instanceof String);
